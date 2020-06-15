7eb45d8dc4355f49fc359870cc13bdccf398a25b
Ilya <tkach.io123@gmail.com>
2020-06-15 19:47:37 +0300

    Added a user input

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import SearchBar from "./SearchBar";
 
 const App = () => {
-    return <div>App</div>
+    return (
+        <div><SearchBar /></div>
+    )
 };
 
 export default App;
